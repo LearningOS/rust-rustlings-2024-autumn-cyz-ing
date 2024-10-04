7c7b5310e0c6c95f010f9e680ce6de3b8d0ba803
cyz-ing <1316769374@qq.com>
2024-10-04 16:09:13 +0800

    cyz-ing

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -48,8 +48,7 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        let unit_like_struct =UnitLikeStruct();
+        let unit_like_struct: UnitLikeStruct=UnitLikeStruct{};
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
