5142d08545350935e1b9817bb35747ee4f5c66e9
cyz-ing <1316769374@qq.com>
2024-10-13 16:36:28 +0800

    cyz-ing

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -32,7 +32,8 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        let value : &mut u32 = &mut *(address as *mut u32);
+        *value = 0xAABBCCDD
     }
 }
 
