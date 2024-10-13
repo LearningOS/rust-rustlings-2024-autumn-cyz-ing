ecfa201b8adb4bc1aa457bcdc84dd8edbe364a1e
cyz-ing <1316769374@qq.com>
2024-10-13 15:27:50 +0800

    solve tests3

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -19,11 +19,11 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(true);
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(false);
     }
 }
